6fb1628911a7d8077fafa4dc6bc4e0c0c767c897
fyui001 <yun.cod@gmail.com>
2020-01-26 22:31:23 +0900

    キャラクター情報を取得するようにした

--- main.py
+++ main.py
@@ -15,29 +15,68 @@ def main():
         PREFIX math: <http://www.w3.org/2005/xpath-functions/math#>
         PREFIX xsd: <https://www.w3.org/TR/xmlschema11-2/#>
 
-        SELECT ?name
+        SELECT ?name ?nameKana ?age ?bust ?waist ?hip ?color ?actor ?title
         WHERE {
-            ?x imas:cv ?name
+            ?x rdfs:label ?name.
+            ?x imas:nameKana ?nameKana.
+            ?x foaf:age ?age.
+            ?x imas:Bust ?bust.
+            ?x imas:Waist ?waist.
+            ?x imas:Hip ?hip.
+            ?x imas:Color ?color.
+            ?x imas:cv ?actor.
+            ?x imas:Title ?title.
+            FILTER (isLiteral(?actor)).
         }
     """)
 
+    sql = "INSERT INTO `{table_name}` (name, nameKana, age, bust, hip, waist, color, voice_actor, title) VALUES".format(table_name=db_conf.TABLE)
+
     sparql.setReturnFormat(JSON)
     sparql_data = sparql.query().convert()
-    voice_actor_lists = []
+    imas_characters = []
     value_lists = []
     db_data = mod.get(db_conf.TABLE)
+    if (len(db_data) != 0):
+        mod.truncate(db_conf.TABLE)
 
+    #insert文を作る
     for row in sparql_data['results']['bindings']:
         if row['name']['type'] == 'literal':
-            voice_actor_lists.append(row['name']['value'])
 
-    if set(db_data) != set(voice_actor_lists):
-        for row in list(set(voice_actor_lists) - set(db_data)):
-            value_lists.append("('{name}')".format(name=row))
+            if row['name']['value'] in {'双葉杏', '佐藤心'}:
+                row['bust']['value'] = 0
+                row['hip']['value'] = 0
+                row['waist']['value'] = 0
+            if row['name']['value'] == '安部菜々':
+                row['age']['value'] = 17
 
-        values = ','.join(value_lists)
-        sql = "INSERT INTO `{table_name}` (name) VALUE {values};".format(values=values, table_name=db_conf.TABLE)
-        mod.insert(sql)
+            sql += "('{name}', '{nameKana}', {age}, {bust}, {hip}, {waist}, '{color}', '{actor}', '{title}'),".format(
+                        name=row['name']['value'],
+                        nameKana=row['nameKana']['value'],
+                        age=row['age']['value'],
+                        bust=row['bust']['value'],
+                        hip=row['hip']['value'],
+                        waist=row['waist']['value'],
+                        color=row['color']['value'],
+                        actor=row['actor']['value'],
+                        title=row['title']['value']
+                    )
+    sql = sql.rstrip(',')
+    sql += ';'
+    mod.insert(sql)
+
+    '''
+        MEMO: 差分更新してやるやつを実装したほうがきれいかなぁ
+        声優だけ撮ってきてた時の差分更新してるやつ
+        if set(db_data) != set(imas_characters):
+            for row in list(set(imas_characters) - set(db_data)):
+                value_lists.append("('{name}')".format(name=row))
+
+            values = ','.join(value_lists)
+            sql = "INSERT INTO `{table_name}` (name) VALUE {values};".format(values=values, table_name=db_conf.TABLE)
+            mod.insert(sql)
+    '''
 
 if __name__ == '__main__':
     main()
--- modules/db_connector.py
+++ modules/db_connector.py
@@ -21,7 +21,15 @@ def table_check(table_name):
     if not tables:
         cur.execute("CREATE TABLE `{table_name}` ("
             "`id` int(11) NOT NULL AUTO_INCREMENT,"
-            "`name` varchar(256) NOT NULL DEFAULT '',"
+            "`name` varchar(255) NOT NULL DEFAULT '',"
+            "`nameKana` varchar(255) NOT NULL DEFAULT '',"
+            "`age` int(11) UNSIGNED NOT NULL DEFAULT 0,"
+            "`bust` float UNSIGNED NOT NULL DEFAULT 0,"
+            "`hip` float UNSIGNED NOT NULL DEFAULT 0,"
+            "`waist` float UNSIGNED NOT NULL DEFAULT 0,"
+            "`color` varchar(40) NOT NULL DEFAULT '',"
+            "`voice_actor` varchar(255) NOT NULL DEFAULT '',"
+            "`title` varchar(50) NOT NULL DEFAULT '',"
             "PRIMARY KEY (`id`)"
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4".format(table_name=table_name)
             )
@@ -38,6 +46,15 @@ def get(table_name):
         for row in cur.fetchall():
             result.append(row[0])
         return result
+    except:
+        raise
+
+def truncate(table_name):
+    sql = 'TRUNCATE TABLE `{table_name}`;'.format(table_name=table_name)
+    cnx = connect()
+    cur = cnx.cursor()
+    try:
+        cur.execute(sql)
     except:
         raise
 
